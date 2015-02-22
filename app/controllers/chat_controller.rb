d941f44ac172512e776aa36436578175c4ecb6db
Matías Menich <matimenich@gmail.com>
2015-02-22 16:37:12 -0300

    fix redis

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -7,7 +7,7 @@ class ChatController < ApplicationController
       redis_thread = Thread.new do
         # Needs its own redis connection to pub
         # and sub at the same time
-        REDIS.subscribe params[:id] do |on|
+        Redis.new(:url => ENV['REDISTOGO_URL'] || "redis://localhost:6379/").subscribe params[:id] do |on|
           on.message do |channel, message|
             tubesock.send_data message
           end
@@ -17,7 +17,7 @@ class ChatController < ApplicationController
       tubesock.onmessage do |m|
         # pub the message when we get one
         # note: this echoes through the sub above
-        REDIS.publish m.split(",")[0], m.split(",")[1]
+        Redis.new(:url => ENV['REDISTOGO_URL'] || "redis://localhost:6379/").publish m.split(",")[0], m.split(",")[1]
       end
       
       tubesock.onclose do
